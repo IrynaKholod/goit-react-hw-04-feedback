b76fd5ba329418912ed5329d96c151e2a3bfe382
IrynaKholod <104202392+IrynaKholod@users.noreply.github.com>
2022-12-21 19:35:42 +0200

    props

--- src/components/Notification/Notification .jsx
+++ src/components/Notification/Notification .jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 // import css from './Notification.module.css';
 
 
@@ -15,5 +16,8 @@ function Notification({ message }) {
 }}>{message}</h2>;
 }
 
+
+Notification.propTypes = {
+message: PropTypes.string.isRequired}
 export default Notification;
 
